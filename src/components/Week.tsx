7ff80c7c0b9f88b866612b66bbf267712aa3a30f
markdstouffer <markdstouffer@gmail.com>
2021-08-24 00:23:01 -0400

    added date heading

--- src/components/Week.tsx
+++ src/components/Week.tsx
@@ -3,15 +3,25 @@ import DailyChecklist from './DailyChecklist';
 import './Week.global.css';
 
 const Week = () => {
+  const d = new Date();
+  const today = new Date(d.getFullYear(), d.getMonth(), d.getDate());
+  const lastSunday = new Date(today.setDate(today.getDate() - today.getDay()));
+  const weekTitle = `Week of Sunday ${new Intl.DateTimeFormat('en-US', {
+    month: 'long',
+  }).format(lastSunday)} ${lastSunday.getDate()}, ${lastSunday.getFullYear()}`;
+
   return (
-    <div id="week_container">
-      <DailyChecklist day="Monday" />
-      <DailyChecklist day="Tuesday" />
-      <DailyChecklist day="Wednesday" />
-      <DailyChecklist day="Thursday" />
-      <DailyChecklist day="Friday" />
-      <DailyChecklist day="Saturday" />
-      <DailyChecklist day="Sunday" />
+    <div>
+      <h1 id="week_title">{weekTitle}</h1>
+      <div id="days_container">
+        <DailyChecklist day="Monday" />
+        <DailyChecklist day="Tuesday" />
+        <DailyChecklist day="Wednesday" />
+        <DailyChecklist day="Thursday" />
+        <DailyChecklist day="Friday" />
+        <DailyChecklist day="Saturday" />
+        <DailyChecklist day="Sunday" />
+      </div>
     </div>
   );
 };
