c7917cd3866137b2d89fea895f996398bb0cee78
Mark Stouffer <markdstouffer@gmail.com>
2021-08-23 22:14:37 -0700

    Weekly Date (#9)
    * added date heading
    
    * date formatting adjustments
    
    * remove redundant date call
    
    * add dates for each weekday + formatting

--- src/components/Week.tsx
+++ src/components/Week.tsx
@@ -3,15 +3,24 @@ import DailyChecklist from './DailyChecklist';
 import './Week.global.css';
 
 const Week = () => {
+  const d = new Date();
+  const lastSunday = new Date(d.setDate(d.getDate() - d.getDay()));
+  const weekTitle = `Week of ${new Intl.DateTimeFormat('en-US', {
+    month: 'long',
+  }).format(lastSunday)} ${lastSunday.getDate()}, ${lastSunday.getFullYear()}`;
+
   return (
-    <div id="week_container">
-      <DailyChecklist day="Monday" />
-      <DailyChecklist day="Tuesday" />
-      <DailyChecklist day="Wednesday" />
-      <DailyChecklist day="Thursday" />
-      <DailyChecklist day="Friday" />
-      <DailyChecklist day="Saturday" />
-      <DailyChecklist day="Sunday" />
+    <div>
+      <h1 id="week_title">{weekTitle}</h1>
+      <div id="days_container">
+        <DailyChecklist day="Monday" />
+        <DailyChecklist day="Tuesday" />
+        <DailyChecklist day="Wednesday" />
+        <DailyChecklist day="Thursday" />
+        <DailyChecklist day="Friday" />
+        <DailyChecklist day="Saturday" />
+        <DailyChecklist day="Sunday" />
+      </div>
     </div>
   );
 };
