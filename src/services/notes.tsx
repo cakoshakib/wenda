a96383a98bb9bae896a482aba5f3b90c30d75f61
markdstouffer <markdstouffer@gmail.com>
2021-09-06 01:44:00 -0400

    create Note type, store 'checked' property on notes

--- src/services/notes.tsx
+++ src/services/notes.tsx
@@ -1,30 +1,31 @@
 import Store from 'electron-store';
+import { Note } from '../types';
 
 const store = new Store({ name: 'current-tasks' });
 
 const getDay = (day: string) => {
-  return store.get(day) as string[];
+  return store.get(day) as Note[];
 };
 
-const addNote = (day: string, note: string) => {
-  let current = store.get(day) as string[];
+const addNote = (day: string, content: string) => {
+  let current = store.get(day) as Note[];
   if (!current) {
     store.set(day, []);
-    current = store.get(day) as string[];
+    current = store.get(day) as Note[];
   }
-  const updated = current.concat(note);
+  const updated = current.concat({ content, checked: false });
   store.set(day, updated);
 };
 
 const deleteNote = (day: string, index: number) => {
-  const dayArr = store.get(day) as string[];
+  const dayArr = store.get(day) as Note[];
   dayArr.splice(index, 1);
   store.set(day, dayArr);
 };
 
 const editNote = (day: string, index: number, newNote: string) => {
-  const dayArr = store.get(day) as string[];
-  dayArr[index] = newNote;
+  const dayArr = store.get(day) as Note[];
+  dayArr[index].content = newNote;
   store.set(day, dayArr);
 };
 
@@ -34,4 +35,17 @@ const archiveNotes = (week: string) => {
   store.store = {};
 };
 
-export default { getDay, addNote, deleteNote, editNote, archiveNotes };
+const toggleChecked = (day: string, index: number) => {
+  const dayArr = store.get(day) as Note[];
+  dayArr[index].checked = !dayArr[index].checked;
+  store.set(day, dayArr);
+};
+
+export default {
+  getDay,
+  addNote,
+  deleteNote,
+  editNote,
+  archiveNotes,
+  toggleChecked,
+};
